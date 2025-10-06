da9718fb5c54ab5bfafaac4b53b10ce769999a7e
Vladyslav Popov <vlad_np@ukr.net>
2025-10-06 18:51:11 +0200

    implemented the promize generator

---
+++ src/js/snackbar.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('promiseForm');
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const delay = Number(form.elements.delay.value);
+    const state = form.elements.state.value;
+
+    // створюємо проміс
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+
+    // обробка результату
+    promise
+      .then(delay => {
+        iziToast.success({
+          title: 'Fulfilled',
+          message: `Fulfilled promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      })
+      .catch(delay => {
+        iziToast.error({
+          title: 'Rejected',
+          message: `Rejected promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      });
+
+    // очищення форми
+    form.reset();
+  });
+});
